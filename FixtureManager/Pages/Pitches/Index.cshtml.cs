e7dffaa54d2014fd1b63de975b67bd17a7270414
euanmacfarlane <euan2@mac.com>
2021-09-17 22:48:10 +0100

    Event to models

--- FixtureManager/Pages/Pitches/Index.cshtml.cs
+++ FixtureManager/Pages/Pitches/Index.cshtml.cs
@@ -7,60 +7,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using FixtureManager.Models;
 using FixtureManager.Data;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace FixtureManager.Pages.Pitches
 {
-
-    public class Event
-    {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public DateTime Start { get; set; }
-        public DateTime End { get; set; }
-        public Guid ResourceId { get; set; }
-        public string Color { get; set; }
-    }
-
-    public class RecurringEvent: Event
-    {
-        public List<int> DaysOfWeek { get; set; } = new List<int>();
-        public string StartTime
-        {
-            get
-            {
-                return (DaysOfWeek.Count() > 0) ? Start.ToString("HH:mm") : "";
-            }
-        }
-
-        public string EndTime
-        {
-            get
-            {
-                return (DaysOfWeek.Count() > 0) ? End.ToString("HH:mm") : "";
-            }
-        }
-    }
-
     public class IndexModel : PageModel
     {
         private readonly FixtureManager.Data.ApplicationDBContext _context;
 
-        public List<Event> Recurring
-        {
-            get
-            {
-                return new List<Event>
-                {
-                    new RecurringEvent { Id = Guid.NewGuid(), Title= "U7, U10, U12 Training",Start=DateTime.Parse("2021-08-29T08:45"), End = DateTime.Parse("2022-05-30T10:15"), Color="black", ResourceId=Guid.Parse("9E365BFE-DDC2-4296-83DF-B278A846207F") , DaysOfWeek = {6 } },
-                    new RecurringEvent { Id = Guid.NewGuid(), Title= "U8, U9 Training",Start=DateTime.Parse("2021-08-29T10:30"), End = DateTime.Parse("2022-05-30T11:45"), Color="black", ResourceId=Guid.Parse("9E365BFE-DDC2-4296-83DF-B278A846207F") , DaysOfWeek = {6 } },
-                    new RecurringEvent { Id = Guid.NewGuid(), Title= "U8, U9 Training",Start=DateTime.Parse("2021-08-29T10:30"), End = DateTime.Parse("2022-05-30T11:45"), Color="black", ResourceId=Guid.Parse("FF4B08C8-DC9A-4F4A-9EC1-BADA343145B9") , DaysOfWeek = {6 } }
-
-                };
-
-
-            }
-        }
 
         public IndexModel(FixtureManager.Data.ApplicationDBContext context)
         {
@@ -85,18 +38,10 @@ namespace FixtureManager.Pages.Pitches
                     .ThenInclude(fa => fa.Pitch)
                 .Include(f => f.FixtureAllocation)
                 .Where(f => f.FixtureAllocation != null && f.FixtureAllocation.Pitch != null)
-                .Select(f => new Event
-                {
-                    Id = f.Id,
-                    Title = $"{f.Team.DisplayName} v {f.Opponent}",
-                    Start = f.FixtureAllocation.Start,
-                    End = f.FixtureAllocation.End,
-                    ResourceId = f.FixtureAllocation.PitchId.Value,
-                    Color = f.Team.Colour
-
-                })
+                .Select(f => new Event(f))  
                 .ToListAsync();
-            Event.AddRange(Recurring);
+
+            Event.AddRange(RecurringEvent.Recurring);
 
             var serializeOptions = new JsonSerializerOptions
             {
