7847c8ec554d1be45e9de35ee989cf741c7389f1
euanmacfarlane <euan2@mac.com>
2021-09-17 17:30:52 +0100

    Fixed EventExists check

--- FixtureManager/Controllers/EventController.cs
+++ FixtureManager/Controllers/EventController.cs
@@ -96,34 +96,36 @@ namespace FixtureManager.Controllers
 
         // POST: api/Event
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Event>> PostEvent(Event @event)
-        {
-            _context.Event.Add(@event);
-            await _context.SaveChangesAsync();
+        //[HttpPost]
+        //public async Task<ActionResult<Event>> PostEvent(Event @event)
+        //{
+        //    //_context.Event.Add(@event);
+        //    //await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetEvent", new { id = @event.Id }, @event);
-        }
+        //    //return CreatedAtAction("GetEvent", new { id = @event.Id }, @event);
+        //}
 
-        // DELETE: api/Event/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteEvent(Guid id)
-        {
-            var @event = await _context.Event.FindAsync(id);
-            if (@event == null)
-            {
-                return NotFound();
-            }
+        //// DELETE: api/Event/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteEvent(Guid id)
+        //{
+        //    //var @event = await _context.Event.FindAsync(id);
+        //    //if (@event == null)
+        //    //{
+        //    //    return NotFound();
+        //    //}
 
-            _context.Event.Remove(@event);
-            await _context.SaveChangesAsync();
+        //    //_context.Event.Remove(@event);
+        //    //await _context.SaveChangesAsync();
 
-            return NoContent();
-        }
+        //    //return NoContent();
+        //}
 
         private bool EventExists(Guid id)
         {
-            return _context.Event.Any(e => e.Id == id);
+            return _context.FixtureAlloctation.Any(fa => fa.FixtureId == id);
         }
+
+
     }
 }
