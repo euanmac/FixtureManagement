c75b5a8e15855fa6177e2bc677694bf5e1f5b16e
euanmacfarlane <euan2@mac.com>
2023-01-02 22:15:01 +0000

    Gameweek

---
+++ FixtureManager/Controllers/FixtureFulltimeController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FixtureManager.Data;
+using FixtureManager.Pages.Pitches;
+using FixtureManager.Models;
+
+//git remote set-url --add https://euanmac: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c@github.com/euanmac/FixtureManagement.git
+//git remote add origin https://euanmac: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c@github.com/euanmac/FixtureManagement.git
+
+namespace FixtureManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FixtureFulltimeController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        public FixtureFulltimeController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        //// GET: api/Event
+        //[HttpGet]
+        //public async Task<ActionResult<IList<TeamReconiliationRow>>> Reconcile(Guid teamId, DateTime matchDate)
+        //{
+        //    Team team = await _context.Team.Where(t => t.Id == teamId).FirstAsync();
+
+        //    if (team is null)
+        //    {
+        //        return null;
+        //    }
+
+        //    IList<Fixture> fixtures = await _context.Fixture.Where(f => f.TeamId == team.Id).ToListAsync();
+
+        //    //all match so return
+        //    return team.Rec2(DateOnly.FromDateTime(matchDate), DateOnly.FromDateTime(matchDate));
+
+        //}
+
+
+    }
+}
+
+
+
+//git remote add set-url https://euanmac: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@https://github.com/euanmac/FixtureManagement.git
+
+//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
---
+++ FixtureManager/Models/FixtureRec.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+namespace FixtureManager.Models
+{
+    public class TeamReconiliationRow
+    {
+        public Team Team { get; set; }
+        public FixtureRecMatchType RecStatus { get; set; }
+        [DataType(DataType.Date)]
+        public DateOnly MatchDate { get; set; }
+        public bool IsAllocated { get; set; }
+        public string Venue { get; set; }
+        public string Opponent { get; set; }
+        public string Pitch { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime Start { get; set; }
+        public string FullTimeURL { get; set; }
+        public FixtureType FixtureType { get; set; }
+    }
+
+    public enum FixtureRecMatchType
+    {
+        unknownTeam,
+        noFixture,
+        localFixtureOnly,
+        localFixtureUnmatched,
+        fullTimeUnmatched,
+        fullTimematched,
+    }
+}
